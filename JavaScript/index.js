c817cd5b47fcbb02e2204a7e932c03ffd5d3df14
Dimaetor <53823825+Coskntkk@users.noreply.github.com>
2022-03-19 10:05:11 +0300

    Update index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -2,22 +2,17 @@
 let testCases = [6, 24, 125, 720, 1024, 40320];
 
 // Create a recursive function that tests if a number is the exact upper bound of the factorial of n. If so, return an array of the exact factorial bound and n, or otherwise, the string "Not an exact bound!"
-// Finde exact upper bound
-function isFactorial(n) {
-  let start = n;
-  for (var i = 1; ; i++) {
-    if (n % i == 0) {
-      n = parseInt(n / i);
-    } else {
-      break;
-    }
+// Finde exact upper bound recursive
+function isFactorial(num, i = 1, start = num) {
+  num = num / i;
+  if (num == 1) {
+    return [start, i];
   }
-
-  if (n == 1) {
-    return [start, i-1];
-  } else {
+  if (num < 1) {
     return "Not an exact bound!";
   }
+  i++;
+  return isFactorial(num, i, start);
 }
 
 // Tests the input against the function
